52edb7fd73b9e5015b007dd36cd91b09b2de9769
Arie Prasetyo <arieprasetyo@192.168.1.6>
2021-02-22 12:59:41 +0700

    Finished styling Body and cells

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { GenerateCells } from "../../utils";
 import NumberDisplay from "../NumberDisplay";
-
+import Button from "../Button"
 import "./App.scss";
+import { Cell, CellState } from "../../types";
+
+
+
 const App: React.FC = () => {
+  const [cells, setCells] = useState<Cell[][]>(GenerateCells());
+
+  const renderCells = () : React.ReactNode => {
+    return cells.map((row: any[], rowIndex: any) =>
+    row.map((_cell: any, colIndex: any) =>
+     <Button key={`${rowIndex}-${colIndex}`} />)
+    )
+  };
+    
   return (
     <div className="App">
       <div className="Header">
         <NumberDisplay value={0} />
         <div className="Face">
-           <span role="img" aria-label="face">
-           😸
-           </span>
-           </div>
-        <NumberDisplay value={0} />
-       </div>
-      <div className="Body">Body</div>
+          <span role="img" aria-label="face">
+            😸
+          </span>
+         </div>
+         <NumberDisplay value={0} />
+      </div>
+      <div className="Body">{renderCells()}</div>
 
     </div>
   );
